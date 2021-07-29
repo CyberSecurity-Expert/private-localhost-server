779ce19a98707de57c1d09e1e6d1c3bacf8ed1c1
CyberSecurity Expert <86741499+CyberSecurity-Expert@users.noreply.github.com>
2021-07-29 14:30:48 -0400

    Create server.py

---
+++ Server/server.py
@@ -0,0 +1,9 @@
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Your server is being hosted at port",PORT)
+    httpd.serve_forever()
